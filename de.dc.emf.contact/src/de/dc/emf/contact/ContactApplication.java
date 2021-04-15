5153ff5c9f26910a1fc3cde54495e6579dbefad0
chqu1012 <chqu1012@mail.com>
2021-04-15 19:24:07 +0200

    Refactored Application class

--- de.dc.emf.contact/src/de/dc/emf/contact/ContactApplication.java
+++ de.dc.emf.contact/src/de/dc/emf/contact/ContactApplication.java
@@ -1,23 +1,9 @@
 package de.dc.emf.contact;
 
-import com.google.inject.Inject;
-
-import de.dc.emf.fx.workbench.jmetro.core.di.IEmfPluginService;
-import de.dc.emf.fx.workbench.jmetro.properties.service.IPropertyService;
 import de.dc.emf.fx.workbench.jmetro.ui.EmfApplication;
-import de.dc.emf.fx.workbench.jmetro.ui.EmfWorkbench;
 
 public class ContactApplication extends EmfApplication{
 
-	@Inject IPropertyService propertyService;
-	@Inject IEmfPluginService pluginService;
-	
-	@Override
-	protected void initWorkbench(EmfWorkbench workbench) {
-		propertyService.initializeProperties();
-		pluginService.init(workbench);
-	}
-	
 	public static void main(String[] args) {
 		launch(args);
 	}
